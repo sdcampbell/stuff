40073c6f186108ae84c76bef9611214cd68f59d2
Steve Campbell <sdcampbell68@live.com>
2017-12-21 15:18:00 -0500

    Update sendloginalert.sh

--- scripts/sendloginalert.sh
+++ scripts/sendloginalert.sh
@@ -4,7 +4,7 @@ while :
 do
 	if grep -qF "SUCCESSFUL" /root/msfspool.txt
 	then
-		sendEmail -f steven.d.campbell68@gmail.com    -t steven_campbell@rapid7.com -u "owa_login SUCCESSFUL!" -m "Check your msfspool.txt log" -s smtp.gmail.com:587 -o tls=yes -xu steven.d.campbell68@gmail.com -xp $1
+		sendEmail -f $1 -t $3 -u "owa_login SUCCESSFUL!" -m "Check your msfspool.txt log" -s smtp.gmail.com:587 -o tls=yes -xu $1 -xp $2
 		sleep 15m
 	fi
 done
